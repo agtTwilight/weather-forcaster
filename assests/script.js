3b6d9b4d884ef2a8357c07b3c97d3077e39a38ef
agtTwilight <lukasmacmillen@gmail.com>
2023-01-11 22:29:23 -0800

    geocode function displays data and stores locally

--- assests/script.js
+++ assests/script.js
@@ -1,7 +1,7 @@
 // 
 // Weather API data collection & sorting
 //
-var weatherDisplayEls = document.querySelectorAll(".weatherDisplay")
+var weatherListEls = document.querySelectorAll(".weatherList")
 
 // Fetch the weather API, collect the data for the given location for today and the next 5 days (all at noon). Select temp, wind, humidity, and icon, and store them as global variables.
 function getWeatherData(lat, lon) {
@@ -15,39 +15,81 @@ function getWeatherData(lat, lon) {
                         });
                 })
 }
-getWeatherData("45.5152", "-122.6784")
 
-// TODO make sure the li's dont have bullets
-// TODO instead of count, let's just do the easiest thing and make an array of values separted by 8 and ending at 39 (cnt is capped at 40 for the API!)
-// TODO (dreamstate) take averages for each day and have those be displayed instead.
 function displayForecast(test) {
-        var count = 0
-        for (i = 0; i < weatherDisplayEls.length; i++) {
-                var currentDisplay = weatherDisplayEls[i]
-                var temp = "Temp: " + test.list[count].main.temp
-                var wind = "Wind: " + test.list[count].wind.speed
-                var humidity = "Humidity: " + test.list[count].main.humidity
+        var indices = [0, 8, 16, 24, 32, 39]
+        for (i = 0; i < weatherListEls.length; i++) {
+                var currentList = weatherListEls[i]
+
+                // remove children on additional searches
+                while (currentList.children.length > 0) {
+                        currentList.removeChild(currentList.firstChild)
+                }
+
+                var temp = "Temp: " + test.list[indices[i]].main.temp
+                var wind = "Wind: " + test.list[indices[i]].wind.speed
+                var humidity = "Humidity: " + test.list[indices[i]].main.humidity
                 var arr = [temp, wind, humidity]
-                count = count + 7
+
                 for (x = 0; x < arr.length; x++){
                         var li = document.createElement("li")
                         li.textContent = arr[x]
-                        currentDisplay.append(li)
+                        currentList.append(li)
                 }
         }
 }
 
-// TODO get the geocoder working. Use the user input field (change to form submission) and add submit event listener. On submit: 1) run geocoder with input data. 2) save geocode outputs to local storage and on button Seattle that appears below in history. 3) use saved outputs to update weather display & run display forecast function. $) add an if checker so api calls aren't repeated if the city has already been called that day. 
+
+// TODO get the geocoder working. Use the user input field (change to form submission) and add submit event listener. On submit: 1) run geocoder with input data. 2) save geocode outputs to local storage and on button Seattle that appears below in history. 3) use saved outputs to update weather display & run display forecast function. $) add an if checker so api calls aren't repeated if the city has already been called that day.
         // 
         // Geocoding
         // 
-        var geocodeRequestUrl = `http://api.openweathermap.org/geo/1.0/direct?q=Seattle&appid=7013ba507325d4539fec0418a1dfc028`
+var userFormEl = document.querySelector("#search")
 
+userFormEl.addEventListener("click", formSubmitHandler);        
+
+function formSubmitHandler() {
+        var cityNameEl = document.querySelector("#cityName");
+        cityNameValue = cityNameEl.value.trim()
+        // need an if checker to see if cityNameValue is in a button, if not, make it a button--needs to be here to avoid unnecessary function calls.
+        getGeocodeData(cityNameValue)
+}
+
+
+// Get data
+function getGeocodeData(cityName) {
+        // check to see if cityName is already in local storage
+        var geocodeRequestUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=7013ba507325d4539fec0418a1dfc028`
+        
         fetch(geocodeRequestUrl)
         .then(function(response) {
+                // need a response error checker in case the city isnt in the api
                 console.log(response);
                 response.json().then(function (data) {
-                        console.log(data)
-                        // displayForecast(data);
+                        let name = data[0].name
+                        let lat = data[0].lat
+                        let lon = data[0].lon
+
+                        getWeatherData(lat, lon)
+                        createSearchButton(name, lat, lon)
                 });
         })
+}
+
+var searchHistoryEl = document.querySelector("#searchHistory")
+var searchHistoryData = {}
+
+function createSearchButton(name, lat, lon) {
+        searchHistoryData[name] = [lat, lon]
+        localStorage.setItem("searchHistoryData", JSON.stringify(searchHistoryData))
+
+        var btn = document.createElement("button")
+        btn.setAttribute("id", name)
+        btn.textContent = name
+        searchHistoryEl.append(btn)
+        btn.addEventListener("click", function(){
+                var storage = JSON.parse(localStorage.getItem("searchHistoryData"))
+                var key = this.getAttribute("id")
+                console.log(storage[key])
+        })
+}
